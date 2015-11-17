7ef11ab3658d0c4df794d06cb17a8b14c8df71f5
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2015-11-17 17:59:06 +0100

    general fixes

--- Classifiers.R
+++ Classifiers.R
@@ -3,7 +3,8 @@ library(neuralnet)
 library(dplyr)
 library(NeuralNetTools)
 library(caret)  ## this package is used for confusionMatrix which 
-                ## gives information about the predicitive performances of the model
+                ## gives information about the predicitive 
+                ## performances of the model
 gradd_logit <- function(y, X, theta0, epsilon = 1e-07, a = .1, 
                         lambda = 0, debug = FALSE, max_iter = 4000) {
   ## X is n x k matrix
@@ -36,10 +37,8 @@ gradd_logit <- function(y, X, theta0, epsilon = 1e-07, a = .1,
 
 predict_logit <- function(X, theta, cutoff = 0.5) {
   h <- 1/(1+exp(-X%*%theta))
-  ifelse(h>0.5, 1, 0)
+  ifelse(h>cutoff, 1, 0)
 }
-
-
 
 dt <- read.csv("~/Google Drive/MABDA shared/train.csv")
 dt_ <- read.csv("~/Google Drive/MABDA shared/test.csv")
@@ -58,10 +57,16 @@ dt_all <- rbind(dt, dt_)
 ## Do this on both the training and test dataset so that we can make a prediction
 ## directly on the test datset arranged as the train dataset
 
-dtm <- model.matrix(~income+age+race+sex+education, data = dt_all)
+dtm  <- model.matrix(~income+age+race+sex+education+capital.gain+capital.loss+fnlwgt+workclass, data = dt_all)
+
+dtm_ <- model.matrix(~income+age+I(age^2)+I(age^3)+race+sex+education+capital.gain+capital.loss+fnlwgt+I(fnlwgt^2)+I(fnlwgt^3)+workclass, data = dt_all)
 
 train <- dtm[1:n_train,]
 test <- dtm[-c(1:n_train),]
+
+train_ <- dtm_[1:n_train,]
+test_  <- dtm_[-c(1:n_train),]
+
 
 X_train <- train[,-2]
 y_train <- train[,2]
@@ -69,36 +74,52 @@ y_train <- train[,2]
 X_test <- test[,-2]
 y_test <- test[, 2]
 
+X_train_ <- train_[,-2]
+y_train_ <- train_[,2]
+
+X_test_ <- test_[,-2]
+y_test_ <- test_[, 2]
+
 
 ## Here both binary and continuous variables are scaled (X-m)/s. 
 ## A better approach would be to center and scale continuous variables and
 ## encode binary input as {-1, 1} instead of {0, 1}
 
 ## procValue
-procValues    <- preProcess(X_train[idx_continuous,-1], method = c("center", "scale"))
+procValues    <- preProcess(X_train[,-1], 
+                            method = c("center", "scale"))
 X_scaledTrain <-  cbind(1, predict(procValues, X_train[,-1]))
 X_scaledTest  <-  cbind(1, predict(procValues, X_test[,-1]))
 
+procValues    <- preProcess(X_train_[,-1], 
+                            method = c("center", "scale"))
+X_scaledTrain_ <-  cbind(1, predict(procValues, X_train_[,-1]))
+X_scaledTest_  <-  cbind(1, predict(procValues, X_test_[,-1]))
+
+
+
 ## Calculate logistic using GLM --- this has only few regressors --- 
-## you should try to increase the number of regressors, in particular adding nonlinear term, 
+## you should try to increase the number of regressors, in particular 
+## adding nonlinear term, 
 ## e.g. I(age^2), I(age^4)) and adding other continuous variables.
-glm_1 <- glm.fit(X_scaledTrain, y_train, family = binomial(link = "logit"))
+glm_1 <- glm.fit(X_scaledTrain_, y_train, family = binomial(link = "logit"))
 
-fitted_glm <- predict_logit(X_scaledTrain, coef(glm_1))
+fitted_glm <- predict_logit(X_scaledTrain_, coef(glm_1))
 
-## This should converge in one iteration, since the glm coefficient set the gradient == 0
-coeff <- gradd_logit(y_train, X_scaledTrain, a = 0.9, 
+## This should converge in one iteration, since the glm 
+## coefficient set the gradient == 0
+coeff <- gradd_logit(y_train, X_scaledTrain_, a = 0.9, 
                      theta0 = coef(glm_1), debug = TRUE, 
                      epsilon = 1e-10, max_iter = 20000)
 
 ## This is calculate the regularized version. Try to change lambda to see what 
 ## is the effect of it. Ideally, you want to do this on the train dataset
-coeff_reg <- gradd_logit(y_train, X_scaledTrain, a = .9, 
+coeff_reg <- gradd_logit(y_train, X_scaledTrain_, a = .9, 
                      theta0 = coef(glm_1), debug = TRUE, 
-                     epsilon = 1e-10, max_iter = 20000, lambda = 0.01)
+                     epsilon = 1e-10, max_iter = 20000, lambda = 0.001)
 
 ## Make FITTED with the regularized model
-fitted_reg <- predict_logit(X_scaledTrain, coeff_reg)
+fitted_reg <- predict_logit(X_scaledTrain_, coeff_reg)
 
 ## Calculate the FITTED statistics
 confusionMatrix(fitted_glm, y_train)
@@ -106,8 +127,8 @@ confusionMatrix(fitted_reg, y_train)
 
 
 ## Calculate PREDICTION
-predict_reg <- predict_logit(X_scaledTest, coeff_reg)
-predict_glm <- predict_logit(X_scaledTest, coef(glm_1))
+predict_reg <- predict_logit(X_scaledTest_, coeff_reg)
+predict_glm <- predict_logit(X_scaledTest_, coef(glm_1))
 
 ## Calculate statistics of the PREDICTION
 confusionMatrix(predict_glm, y_test)
@@ -119,9 +140,9 @@ confusionMatrix(predict_reg, y_test)
 
 ## Notice that size is the number of neuron in the hidden layer
 ## in this case 4. "entropy = TRUE" us
-out <- nnet(X_scaledTrain, y_train, size = 14, entropy = TRUE, maxit = 1000)
+out <- nnet(X_scaledTrain[,-c(1)], y_train, size = 22, entropy = TRUE, maxit = 1000)
 plotnet(out)
-nn_pred <- ifelse(predict(out, newdata = X_scaledTest)>0.5, 1, 0)
+nn_pred <- ifelse(predict(out, newdata = X_scaledTest[,-c(1)])>0.5, 1, 0)
 confusionMatrix(nn_pred, y_test)
 
 ## This step create a formula object including all the variables in train
@@ -143,18 +164,25 @@ formula <- formula(paste0("income~",  paste0("`", colnames(X_scaledTrain)[-c(1)]
 ## it takes 11 minutes. NOTE: startweights = array(0, 1200) --> this is initializing the weights 
 ## to 0. If you don't this you may have serious convergence problems.
 
-nn1 <- neuralnet(formula, data = cbind(income= y_train, X_scaledTrain), 
+nn1 <- neuralnet(formula, data = cbind(income= y_train, X_scaledTrain), threshold = .0003, 
                  hidden = 14, err.fct = "ce", 
-                 lifesign = "full", linear.output = FALSE, startweights = array(0, 1200))
+                 lifesign = "full", linear.output = FALSE, 
+                 startweights = coef(out))
 
-## plot(nn1)  this will plot the graphic representation of the neural net
+nn2 <- neuralnet(formula, data = cbind(income= y_train, X_scaledTrain), threshold = .03, 
+                 hidden = c(14, 6), err.fct = "ce", 
+                 lifesign = "full", linear.output = FALSE, 
+                 startweights = coef(out))
+
+## plot(nn1)  this will plot the graphic representation of 
+## the neural net
 plotnet(nn1)
 
 fitted_nn <- neuralnet::compute(nn1, X_scaledTrain[,-1])
 fitted_nnet <- ifelse(fitted_nn$net.result>0.5, 1, 0)
 confusionMatrix(fitted_nnet, y_train)
 
-fitted_nn <- neuralnet::compute(nn1, X_scaledTest[,-1])
-fitted_nnet <- ifelse(fitted_nn$net.result>0.5, 1, 0)
-confusionMatrix(fitted_nnet, y_test)
+predicted_nn <- neuralnet::compute(nn1, X_scaledTest[,-1])
+predicted_nnet <- ifelse(predicted_nn$net.result>0.455, 1, 0)
+confusionMatrix(predicted_nnet, y_test)
 
